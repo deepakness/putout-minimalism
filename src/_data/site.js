66c50802f4548111f9dfa6ff590d4f3c7a5027a1
deepakness <deepakkr342@gmail.com>
2024-07-05 11:09:40 +0530

    updated description

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,7 @@ module.exports = {
   title: "Minimalism",
   longTitle: "Thoughts on Living a Minimalist Life",
   author: "Leo Babauta",
-  description: "Leo Babauta, the author of the famous blog Zen Blog, talks about how he lives a minimalist life.",
+  description: "Selected writings of Leo Babauta, the author of the famous blog Zen Blog, on how he lives a minimalist life.",
   url: "https://minimalism.putout.org",
   coverImage: "/assets/cover.png",
   
